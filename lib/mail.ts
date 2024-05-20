fc4f864dc9817164cd5c9efef6ad002dcd2c96e4
Abhishek Sharma <sharma312006@gmail.com>
2024-05-20 09:57:01 +0530

    change confirm link

--- lib/mail.ts
+++ lib/mail.ts
@@ -22,7 +22,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 };
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
